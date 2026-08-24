5db743ac97bcd69a955bb653b0ba1f7ec23a526d
Camilo <contrerascamilo2611@gmail.com>
2026-08-24 16:49:10 -0500

    Pagina Actualizada nuevamente

--- Agrored_web/Agrored_web/static/js/actions_productos.js
+++ Agrored_web/Agrored_web/static/js/actions_productos.js
@@ -1,21 +1,8 @@
-    function validaringreso() {
-        var email = document.getElementById('email').value;
-        var contrasena = document.getElementById('contrasena').value;
-
-        // 1. Ningún campo obligatorio puede estar vacío
-        if (email == '' || contrasena == ''){
-                Swal.fire({
-                title: "Ingresa todos los campos",
-                icon: "error",
-                draggable: true
-                });
-                return false;
-            }else{
-                Swal.fire({
-                title: "Bienvenido " + email + "!",
-                icon: "success",
-                draggable: true
-                });
-                return true;
-            }
-    }
+function Pruducto_agregado(){
+    Swal.fire({
+        title: "Producto Agregado al carrito",
+        icon: "success",
+        draggable: true
+        });
+    return false;
+}

d91e7cc46ec00b7fa2e25544874b838ed78c7e66
hyntg09-a11y <hyntg09@gmail.com>
2026-08-25 07:56:14 -0500

    Actualizacion 3

---
+++ Agrored_web/Agrored_web/static/js/actions_eliminar.js
@@ -0,0 +1,8 @@
+function eliminarproductos(){
+    Swal.fire({
+        title: "¡Quieres eliminar este producto!",
+        icon: "warning",
+        draggable: true
+        });
+    return false;
+}
